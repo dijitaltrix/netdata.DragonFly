4ff3614cc6d81ac68e00c3e98d64f9c21e16ef1c
Ian <ian@dijix.com>
2019-06-08 12:32:44 +0100

    Updated patch

--- dragonfly/patch-libnetdata_libnetdata.h
+++ dragonfly/patch-libnetdata_libnetdata.h
@@ -1,30 +1,31 @@
 --- libnetdata/libnetdata.h	2019-05-22 17:46:15.000000000 +0100
-+++ -	2019-06-08 12:07:07.000000000 +0100
-@@ -30,7 +30,7 @@
++++ -	2019-06-08 11:45:29.000000000 +0100
+@@ -30,24 +30,22 @@
  
  #else /* !defined(ENABLE_JEMALLOC) && !defined(ENABLE_TCMALLOC) */
  
--#if !(defined(__FreeBSD__) || defined(__APPLE__))
-+#if (!(defined(__FreeBSD__) || !(defined(__DragonFly__)) || defined(__APPLE__))
+-#if defined(__DragonFly__)
+-#include <sys/malloc.h>
+-#elif !(defined(__FreeBSD__) || defined(__APPLE__))
++#if !(defined(__FreeBSD__) || defined(__APPLE__))
  #include <malloc.h>
- #endif /* __FreeBSD__ || __APPLE__ */
+-#endif /* __APPLE__ || __FreeBSD__ */
++#endif /* __FreeBSD__ || __APPLE__ */
  
-@@ -38,7 +38,7 @@
+ #endif /* !defined(ENABLE_JEMALLOC) && !defined(ENABLE_TCMALLOC) */
  
  // ----------------------------------------------------------------------------
  
--#if defined(__FreeBSD__)
-+#if (defined(__FreeBSD__) || defined(__DragonFly__))
+-#if (defined(__FreeBSD__) || defined(__DragonFly__))
++#if defined(__FreeBSD__)
  #include <pthread_np.h>
  #define NETDATA_OS_TYPE "freebsd"
  #elif defined(__APPLE__)
-
-@@ -80,7 +84,7 @@
- #include <sys/un.h>
- #include <time.h>
- #include <unistd.h>
--#include <uuid/uuid.h>
-+//#include <uuid/uuid.h>
-
- #ifdef HAVE_NETINET_IN_H
- #include <netinet/in.h>
+ #define NETDATA_OS_TYPE "macos"
+ #else
+ #define NETDATA_OS_TYPE "linux"
+-#endif /* __APPLE__, __DragonFly__, __FreeBSD__*/
++#endif /* __FreeBSD__, __APPLE__*/
+ 
+ #include <pthread.h>
+ #include <errno.h>
